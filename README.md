c31da0db4c893f4041e704f7c7f9a089d28a0cd4
Dimonyga <Dimonyga@gmail.com>
2015-03-30 01:38:39 +0900

    add ini parser

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mysql_galera_checker
 mysql_galera_checker
+
+Requirement
+
+mysql-connector-python
+python-iniparse
--- mysqlchk.py
+++ mysqlchk.py
@@ -1,45 +1,58 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import MySQLdb
+from mysql.connector import MySQLConnection, Error
+from iniparse import ConfigParser
 
 PORT_NUMBER = 8080
-MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
+MYSQL_CONFIG = '/etc/my.cnf.d/mysql-clients.cnf'
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-	
-	#Handler for the GET requests
+def read_db_config(filename='/etc/my.cnf', section='mysql'):
+	parser = ConfigParser()
+	parser.read(filename)
+	db = {}
+	if parser.has_section(section):
+		items = parser.items(section)
+		for item in items:
+			db[item[0]] = item[1]
+	else:
+		raise Exception('{0} not found in the {1} file'.format(section, filename))
+	return db
+
+def connect():
+	db_config = read_db_config(MYSQL_CONFIG)
+	try:
+		conn = MySQLConnection(**db_config)
+	except Error as error:
+		print(error)
+	return conn
+
+class mysqlchk(BaseHTTPRequestHandler):
+
+	db = connect()
+	mysql = db.cursor()
+
 	def do_GET(self):
 		sql= "SHOW STATUS LIKE 'wsrep_local_state';"
-		mysql.execute (sql)
-		#state = mysql.fetchone()
-		#self.wfile.write(state)
-		#print state
-		row = mysql.fetchone()
-		#print row[0], "-->", row[1]
+		if self.db.is_connected() == 0:
+			self.db = connect()
+			self.mysql = self.db.cursor()
+			print ('reconnect to mysql')
+		self.mysql.execute (sql)
+		row = self.mysql.fetchone()
 		if row[1] == '4':
-		    self.send_response(200)
-		    self.send_header('Content-type','text/html')
-		    self.end_headers()
-		    self.wfile.write("OK")
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write("OK")
 		else:
-		    self.send_response(503)
-		    self.send_header('Content-type','text/html')
-		    self.end_headers()
-		    self.wfile.write("ERR")
+			self.send_response(503)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write("ERR")
 		return
-
 try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	db = MySQLdb.connect(host="localhost", user=MYSQL_USER, passwd=MYSQL_PASSWORD, db="mysql", charset='utf8')
-	mysql = db.cursor()
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	server = HTTPServer(('', PORT_NUMBER), mysqlchk)
 	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
